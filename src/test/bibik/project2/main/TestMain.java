fdfce96d375f6a86a018738b87ff3586e70ba70a
Maksim_Bibik <Maksim_Bibik@EPBYMINW0825.minsk.epam.com>
2017-06-26 14:05:07 +0300

    new project

---
+++ src/test/bibik/project2/main/TestMain.java
@@ -0,0 +1,18 @@
+package test.bibik.project2.main;
+
+import static org.junit.Assert.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+
+public class TestMain {
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
